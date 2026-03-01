7375a0721b5da74cba61af9ce297ac9a54eef87d
KaiCACO <mysticbiscuit@cascadesacademy.org>
2026-03-01 14:13:22 -0800

    Update IntakeSubsystem.java

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -73,11 +73,11 @@ public class IntakeSubsystem extends SubsystemBase{
 
     public void boxControls(boolean extendBox, boolean retractBox) {
         if(extendBox && m_lowerLimit.get()) {
-            m_armExtenderOne.set(-0.1);
-            m_armExtenderTwo.set(0.1);
+            m_armExtenderOne.set(-0.2);
+            m_armExtenderTwo.set(0.2);
         } else if (retractBox && m_upperLimit.get()) {
-            m_armExtenderOne.set(0.4);
-            m_armExtenderTwo.set(-0.4);
+            m_armExtenderOne.set(0.2);
+            m_armExtenderTwo.set(-0.2);
         } else {
             m_armExtenderOne.set(0);
             m_armExtenderTwo.set(0);
