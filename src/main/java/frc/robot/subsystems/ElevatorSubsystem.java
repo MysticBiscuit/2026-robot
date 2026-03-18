05a0ee5f50623c79017bc041573773cfd2d57656
KaiCACO <mysticbiscuit@cascadesacademy.org>
2026-03-18 11:16:58 -0700

    Update ElevatorSubsystem.java

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -159,10 +159,10 @@ public class ElevatorSubsystem extends SubsystemBase{
 
    public void elevatorSlideCommand(boolean elevatorSlideOutRequested, boolean elevatorSlideInRequested) {
       if (elevatorSlideOutRequested && m_elevatorSliderBackLimit.get()) {
-         m_elevatorSlider.set(1);
+         m_elevatorSlider.set(0.75);
 
       } else if (elevatorSlideInRequested && m_elevatorSliderFrontLimit.get()) {
-         m_elevatorSlider.set(-1);
+         m_elevatorSlider.set(-0.75);
       } else {
          m_elevatorSlider.set(0);
       }
